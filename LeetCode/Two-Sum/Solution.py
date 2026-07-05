74c8389dd0b17b8f25f214ef169d929f8b2d85fe
Nithish K <90969432+nithish1910k@users.noreply.github.com>
2026-07-06 01:50:29 +0530

    Add solution for Two Sum

---
+++ LeetCode/Two-Sum/Solution.py
@@ -0,0 +1,7 @@
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        for i in range(len(nums)):
+4            for j in range(i + 1, len(nums)):
+5                if (i != j and nums[i] + nums[j] == target):
+6                    return [i, j]
+7        return []
